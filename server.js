bdb1d6ab84195527c986410325aad5ea9c5b2c54
Om Kamble <OmKamble>
2023-03-03 23:25:51 +0530

    third commit

--- server.js
+++ server.js
@@ -74,3 +74,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
