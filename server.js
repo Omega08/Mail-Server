bc252ed0ab8f5583897a70fbcfec520f230c4503
Om Kamble <OmKamble>
2023-03-05 00:51:02 +0530

    commit

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 app.use(bodyParser.json());
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
